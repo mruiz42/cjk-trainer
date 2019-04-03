1ecca5f1a981fe51fc2761a7c5c607de19229ff0
Michael Ruiz <mruiz@tuta.io>
2019-04-03 04:39:27 -0700

    last push before rebranching to restructure and separate Header/Subclass files

--- cjk-trainer/py/ConfirmDeleteTable.py
+++ cjk-trainer/py/ConfirmDeleteTable.py
@@ -13,17 +13,13 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_ConfirmDeleteTableDialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Confirm Delete Table")
-        Dialog.resize(377, 215)
+        Dialog.resize(380, 220)
         self.gridLayout = QtWidgets.QGridLayout(Dialog)
         self.gridLayout.setObjectName("gridLayout")
         self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.No|QtWidgets.QDialogButtonBox.Yes)
         self.buttonBox.setObjectName("buttonBox")
-
-        # ADD
-        self.buttonBox.accepted.connect(self.accepted)
-
         self.gridLayout.addWidget(self.buttonBox, 2, 0, 1, 1)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
@@ -39,7 +35,6 @@ class Ui_ConfirmDeleteTableDialog(object):
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem1)
         self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
-
         self.retranslateUi(Dialog)
         QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), Dialog.accept)
         QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), Dialog.reject)
@@ -50,6 +45,3 @@ class Ui_ConfirmDeleteTableDialog(object):
         self.label.setText(QtWidgets.QApplication.translate("Dialog", "Are you sure you want to delete the table:\n"
 " \n"
 "THIS CANNOT BE UNDONE ", None, -1))
-
-    def accepted(self):
-        self.mainWindow.refreshTableList()
--- cjk-trainer/py/callConfirmDeleteTable.py
+++ cjk-trainer/py/callConfirmDeleteTable.py
@@ -1,12 +1,19 @@
 from py.ConfirmDeleteTable import *
+from py.MainWindow import *
 class ConfirmDeleteTable(QtWidgets.QDialog):
 
-    def __init__(self, mainWindow):
-        #super().__init__()
-        super().__init__(mainWindow)
+    def __init__(self, mainWindow, parent=None):
+        super(ConfirmDeleteTable, self).__init__(parent)
         self.cDTD  = Ui_ConfirmDeleteTableDialog()
+        self.mainWindow = mainWindow
         self.cDTD.setupUi(self)
+        self.cDTD.buttonBox.accepted.connect(self.accepted2)
 
 
     def setTableName(self, tableName):
         self.cDTD.label.setText("Are you sure you want to delete the table:\n"+ tableName + "\nTHIS CANNOT BE UNDONE ")
+
+    def accepted2(self):
+        #TODO RENAME NECESSARY -- CONFLICTING FUNCTION NAME
+        print("hi")
+        self.mainWindow.refreshTableList()
